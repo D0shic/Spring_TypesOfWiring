a2dfb9a11ca64743e694c88962c772b03d33e443
Yakutenko Dasha <yakutenkodasha@gmail.com>
2024-07-15 17:00:12 +0400

    Second commit

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,7 @@
 package koschei.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
-@Component
 public class Egg6 {
 
     @Autowired
@@ -17,4 +15,5 @@ public class Egg6 {
     public String toString() {
         return ", в яйце иголка " + needle.toString();
     }
+
 }
